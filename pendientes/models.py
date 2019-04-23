85cdc6ed71c6b9c866f12c0f0240433413a800e5
Marcelo Elizeche Landó <melizeche@gmail.com>
2019-04-23 01:52:29 -0400

    se agrego el modelo Tarea

--- pendientes/models.py
+++ pendientes/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True, blank=True)
+    estado = models.BooleanField(default=False)
